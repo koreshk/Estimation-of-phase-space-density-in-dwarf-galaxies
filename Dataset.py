c6d22315768b866a4fd99773cf4e6104c6a90fc6
koreshk <167111198+koreshk@users.noreply.github.com>
2024-07-23 13:43:46 +0300

    Added Dataset code

--- Dataset.py
+++ Dataset.py
@@ -1,16 +1,14 @@
 #####################################################################
-Download_Data = 'no'
+Download_Data = 'yes'
 Data_to_binulator = 'yes'
-Bin_param = 'no'
+Bin_param = 'yes'
 gravsphere_param = 'yes'
 
 
 
 
 
-
-gal_name = 'Carina_dSph'
-
+gal_name = 'Z_126-111'
 
 
 
@@ -23,6 +21,9 @@ gal_name = 'Carina_dSph'
 outdir = './gravsphere-master/My_Data/'
 ########################################################################
 
+from gravsphere_initialise_CVnI import *
+from figures import *
+
 import matplotlib.pyplot as plt
 from astroquery.simbad import Simbad
 import pyvo
@@ -34,8 +35,6 @@ import astropy.units as u
 from astropy.coordinates import SkyCoord
 
 from tqdm import tqdm
-from scipy.optimize import curve_fit as fit
-#from scipy.stats.norm import pdf as gaussian 
 
 if(Download_Data == 'yes'):
     
@@ -44,7 +43,18 @@ if(Download_Data == 'yes'):
     Simbad.add_votable_fields('flux_bibcode(V)')
     
     gal_query = Simbad.query_object(gal_name)
-    gal_query.to_pandas().to_excel(outdir + gal_name +'_galaxy_properties.xlsx')
+    
+    gal_properties = pd.DataFrame({'Name': (gal_query['MAIN_ID'][0])[5:],\
+                                   'RA(ICRS) [h:m:s]': gal_query['RA'][0],\
+                                   'DEC(ICRS) [d:m:s]': gal_query['DEC'][0],\
+                                   'Coordinate reference': gal_query['COO_BIBCODE'][0],\
+                                   'Distance': gal_query['Distance_distance'][0],\
+                                   '[Distance unit]': gal_query['Distance_unit'][0],\
+                                   'Distance reference': gal_query['Distance_bibcode'][0],\
+                                   'Flux(V) [Vega mag]': gal_query['FLUX_V'][0],\
+                                   'Flux reference': gal_query['FLUX_BIBCODE_V'][0],\
+                                   }, index=[0])
+    gal_properties.to_excel(outdir + whichgal.replace(' ','_') +'_galaxy_properties.xlsx', index=False)
     
     if (gal_query['Distance_unit'] == 'kpc'): koeff = 1
     if (gal_query['Distance_unit'] == 'Mpc'): koeff = 1e3
@@ -65,11 +75,10 @@ if(Download_Data == 'yes'):
     
     Data = pd.DataFrame({'id': result_table['child id'], 'bib': result_table['link_bibcode'] ,'R': result_table['dist'] * np.pi / 180 * Distance, 'Dens': np.array([1 for i in range(len(result_table['child id']))]),\
                          'vel': result_table['rvz_radvel'], 'vel_err': result_table['rvz_err'], 'vel_bib': result_table['rvz_bibcode'], 'memb_prob': result_table['membership'], 'otype': result_table['otype'], 'coo_bib': result_table['coo_bibcode']})
-    Data.sort_values(by = 'R', inplace=True)
+    Data.sort_values(by = 'bib', inplace=True)
     Data.reset_index(drop=True, inplace=True)
     
     types = np.unique(Data['otype'])
-    print('types = ', types)
     
     qso_ind = np.where(Data['otype'] == 'QSO')[0]
     Data.drop(qso_ind, inplace = True)
@@ -84,114 +93,120 @@ if(Download_Data == 'yes'):
         Data.drop(ind[:-1], inplace=True)
         Data.reset_index(drop=True, inplace=True)
         Data['bib'][ind[-1]] = ';'.join(bibcodes)
-        
-    Data.to_excel(outdir + gal_name +'.xlsx')
+    
+    Data.sort_values(by = 'R', inplace=True)
+    Data.reset_index(drop=True, inplace=True)
+    
+    result_Data = pd.DataFrame({'id': Data['id'],\
+                                'object reference': Data['bib'],\
+                                'coordinate reference': Data['coo_bib'],\
+                                'r [kpc]': Data['R'],\
+                                'Dens (Membership probability)': Data['Dens'],\
+                                'velocity [km/s]': Data['vel'],\
+                                'velocity uncertainty [km/s]': Data['vel_err'],\
+                                'velocity reference': Data['vel_bib'],\
+                                'object type': Data['otype']})
+    
+    result_Data.to_excel(outdir + whichgal.replace(' ','_') +'.xlsx', index=False)
     print('Complete')
 
 if (Data_to_binulator == 'yes'):
+    whichgal = whichgal.replace(' ','_')
     Data = pd.read_excel(outdir + gal_name + '.xlsx')
     
-    '''    
-    My_Data = pd.DataFrame({'R': Data['R'], 'vel': Data['vel'], 'velerr': Data['vel_err']})
-    My_Data['R'] = My_Data['R']
-    My_Data.drop(np.where(My_Data['vel'].isnull() == True)[0], inplace=True)
-    My_Data.reset_index(drop=True, inplace=True) 
-    
-    plt.hist(Data['vel'], color = 'blue', edgecolor = 'black', bins = int(np.sqrt(len(Data['vel']))))
-    plt.xlabel('v, km/s')
-    plt.ylabel('quantity')    
-    plt.savefig(outdir + gal_name + '_hist.pdf') 
-    plt.cla()
-
-    dgal_kpc = 86.0
-    arcsec = 360./2./np.pi * 60. * 60.
-    arcmin = arcsec / 60.    
-    data_kin_vs = np.genfromtxt('./gravsphere-master/Data/Walker_dwarfs/scl_justin1_spec.dat')
-    gotvz = np.where(data_kin_vs[:,6])[0]
-    Rkin = data_kin_vs[gotvz,4]
-    vz = data_kin_vs[gotvz,6]
-    vzerr = data_kin_vs[gotvz,7]
-    
-    GravData = pd.DataFrame({'R': Rkin*60 ,'vel': vz, 'velerr': vzerr})
-    
-    plt.scatter(My_Data['R'], My_Data['vel'], edgecolors = 'blue', alpha = 0.33 , label = 'MyData')
-    plt.scatter(GravData['R'], GravData['vel'], edgecolors= 'red',s = 0.6, label = 'gravsphere example')
-    plt.legend()
-    plt.xlabel('r, asec')
-    plt.ylabel('v, km/s')
-    plt.savefig(outdir + gal_name + '.pdf')
-    plt.show()
+    '''
+    Data.drop(np.where(Data['r [kpc]'] >= 1.5)[0], inplace=True)
+    Data.reset_index(drop=True, inplace=True) 
+    Data.drop(np.where(Data['object type'] == 'QSO')[0], inplace=True)
+    Data.reset_index(drop=True, inplace=True) 
     '''
     
-    ##################################################################3
+    Data_phot = pd.DataFrame({ 'R': Data['r [kpc]'], 'Dens': Data['Dens (Membership probability)'] })
     
-    Data_phot = pd.DataFrame({ 'R': Data['R'], 'Dens': Data['Dens'] })
+    fig = plt.figure(figsize=(figx,figy))
+    ax = fig.add_subplot(111)
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(mylinewidth)
+    plt.xticks(fontsize=myfontsize)
+    plt.yticks(fontsize=myfontsize)    
+
     Y, X = np.histogram(Data_phot['R'], bins = int(np.sqrt(len(Data_phot['R']))))
-    ind = np.where(Y >= max(Y)/2)[0][0]
     plt.hist(Data_phot['R'], bins = int(np.sqrt(len(Data_phot['R']))), edgecolor = 'black')
+    plt.axhline(max(Y)/2, color = 'black')
+    ind = np.where(Y >= max(Y)/2)[0][0]
     plt.axvline(X[ind], color = 'red', label = 'r =' + str(X[ind]))
     plt.axvline(X[ind+1], color = 'green', label = 'r =' + str(X[ind+1]))
     plt.legend()
-    plt.savefig(outdir + gal_name + '_radius_hist.pdf')    
-
+    
+    plt.xlabel(r'$r\,[{\rm kpc}]$',\
+                   fontsize=myfontsize)
+    plt.ylabel(r'$N_{{\rm stars}}$',\
+                   fontsize=myfontsize)    
+    plt.savefig(outdir + gal_name + '_radius_hist.pdf')
+    
     Data_phot.to_csv(outdir + gal_name + '_phot.csv')
     
-    Data_spec = pd.DataFrame({'R': Data['R'], 'vel': Data['vel'], 'velerr': Data['vel_err']})
+    Data_spec = pd.DataFrame({'R': Data['r [kpc]'], 'vel': Data['velocity [km/s]'], 'velerr': Data['velocity uncertainty [km/s]']})
+    
     Data_spec.drop(np.where(Data_spec['vel'].isnull() == True)[0], inplace=True)
     Data_spec.reset_index(drop=True, inplace=True)
     Data_spec.drop(np.where(Data_spec['velerr'].isnull() == True)[0], inplace=True)
     Data_spec.reset_index(drop=True, inplace=True)
+    
+    '''
+    Data_spec.drop(np.where(Data_spec['vel'] <= -100)[0], inplace=True)
+    Data_spec.reset_index(drop=True, inplace=True)  
+    '''
+    fig = plt.figure(figsize=(figx,figy))
+    ax = fig.add_subplot(111)
+    for axis in ['top','bottom','left','right']:
+        ax.spines[axis].set_linewidth(mylinewidth)
+    plt.xticks(fontsize=myfontsize)
+    plt.yticks(fontsize=myfontsize) 
+    
+    plt.hist(Data_spec['vel'], bins = int(np.sqrt(len(Data_spec['vel']))), edgecolor = 'black')
+    
+    plt.xlabel(r'$v\,[{\rm km/s}]$',\
+                   fontsize=myfontsize)
+    plt.ylabel(r'$N_{{\rm stars}}$',\
+                   fontsize=myfontsize)    
+    plt.savefig(outdir + gal_name + '_vel_hist.pdf')
+    
     Data_spec.to_csv(outdir + gal_name + '_spec.csv')
-    
-    plt.scatter(Data_spec['R'], Data_spec['vel'])
-    plt.show()
-    
-    Data = pd.read_excel(outdir + gal_name + '_galaxy_properties.xlsx')
-    Data['N_memb(phot)'] = len(Data_phot['R'])
-    Data['N_memb(spec)'] = len(Data_spec['R'])
-    
-    Data['Nibn'] = int(np.sqrt(Data['N_memb(phot)']))
-    Data['Nbin_kin'] = int(np.sqrt(Data['N_memb(spec)']))
-    Data.to_excel(outdir + gal_name + '_galaxy_properties.xlsx')
-    
-    
-    
-    '''
-    Data_spec.drop(np.where(Data_spec['vel'] >= high)[0], inplace=True)
-    Data_spec.reset_index(drop=True, inplace=True)
-    Data_spec.drop(np.where(Data_spec['vel'] <= low)[0], inplace=True)
-    Data_spec.reset_index(drop=True, inplace=True)
-    '''
-    
-    '''
-    vel_sys = np.mean(Data_spec['vel'])
-    Data_spec['vel'] = Data_spec['vel'] - vel_sys
-    Data_spec.drop(np.where(Data_spec['vel'] - 3 * Data_spec['velerr'] <= 0)[0], inplace=True)
-    Data_spec.reset_index(drop=True, inplace=True)   
-    Data_spec.to_csv(outdir + gal_name + '_spec.csv')
-    '''
     
 if(Bin_param == 'yes'):
-    Data_phot = pd.read_csv(outdir + gal_name + '_phot.csv')
-    print('Nbin = ', int(np.sqrt( len( Data_phot['R'] ) )))
+    whichgal = whichgal.replace(' ','_')
     
-    Data_spec = pd.read_csv(outdir + gal_name + '_spec.csv')
-    print('Nbinkin = ', int(np.sqrt( len( Data_spec['R'] ) )))
+    Data = pd.read_excel(outdir + gal_name + '_galaxy_properties.xlsx')
+    Data['Number of members (photometric)'] = len(Data_phot['R'])
+    Data['Number of members (kinematic)'] = len(Data_spec['R'])
+    
+    Data['Nbin'] = int(np.sqrt(len(Data_phot['R'])))
+    Data['Nbinkin'] = int(np.sqrt(len(Data_phot['R'])))
+    
+    Data.to_excel(outdir + gal_name + '_galaxy_properties.xlsx', index=False)    
+    
+    print('Nbin = ', Data['Nbin'][0])
+    print('Nbinkin = ', Data['Nbinkin'][0])
     
 if(gravsphere_param == 'yes'):
+    whichgal = whichgal.replace(' ','_')
     Mass_vega = 2.135
     Distance_vega = 7.86e-3
     
     gal = pd.read_excel(outdir + gal_name + '_galaxy_properties.xlsx')
-    Vmag = gal['FLUX_V'][0]
-    Distance = gal['Distance_distance'][0]
-    unit = gal['Distance_unit'][0]
+    Vmag = gal['Flux(V) [Vega mag]'][0]
+    Distance = gal['Distance'][0]
+    unit = gal['[Distance unit]'][0]
     if (unit == 'kpc'): koeff = 1
     if (unit == 'Mpc'): koeff = 1e3
     
     Distance = Distance * koeff
     
     M = Mass_vega * 10**(-Vmag/2.5) * (Distance/Distance_vega)**2
+    
+    gal['Mass [Msun]'] = str(M*1e-6) + 'e6'
+    gal.to_excel(outdir + gal_name + '_galaxy_properties.xlsx', index=False)
     
     print('dgal_kpc = ', Distance)
     print('Mass = ', M*1e-6, 'e6')
